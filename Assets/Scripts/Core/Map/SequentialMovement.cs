8950e692d2f39b9de2a0c15e74f0faabb4981c56
Щукин Клим < >
2016-12-23 17:09:47 +0200

    Player movement

--- Assets/Scripts/Core/Map/SequentialMovement.cs
+++ Assets/Scripts/Core/Map/SequentialMovement.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Core.Map.Pathfinding;
 
 
@@ -8,43 +6,47 @@ namespace Core.Map
 {
 	public class SequentialMovement
 	{
+		#region Private
+
 		private Transform[] _path;
 		private int _currentIndex;
 		private MovableObject _client;
 		private bool _looped;
 
-		public SequentialMovement (Transform[] path, MovableObject client, bool looped = false)
+		#endregion
+
+		public SequentialMovement(Transform[] path, MovableObject client, bool looped = false)
 		{
 			_path = path;
 			_client = client;
 			_looped = looped;
 		}
 
-		public void UpdateMovement ()
+		public void UpdateMovement()
 		{
-			if (_client.CurrentPath.Empty)
+			if(_client.CurrentPath.Empty)
 			{
 				_currentIndex++;
-				if (_currentIndex > _path.Length - 1 && _looped)
+				if(_currentIndex > _path.Length - 1 && _looped)
 				{
 					_currentIndex = 0;
 				}
 				else
-				if (_currentIndex > _path.Length && !_looped)
+				if(_currentIndex > _path.Length && !_looped)
 				{
 					return;
 				}
 
-				BeginMovement ();
+				BeginMovement();
 			}
 		}
 
-		private void BeginMovement ()
+		private void BeginMovement()
 		{
-			_client.BeginMovementByPath (
-				Pathfinder.FindPathToDestination (_client.Map,
-				                                  _client.CurrentNode.GridPosition,
-				                                  _client.Map.GetNodeByPosition (_path [_currentIndex].position).GridPosition));
+			_client.BeginMovementByPath(
+				Pathfinder.FindPathToDestination(_client.Map,
+				                                 _client.CurrentNode.GridPosition,
+				                                 _client.Map.GetNodeByPosition(_path[_currentIndex].position).GridPosition));
 		}
 	}
 }
